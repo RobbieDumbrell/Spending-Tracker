60774aeffbe054e611a1ed63b2c91afe6db59b98
Robbie Dumbrell <robert.dumbrell@outlook.com>
2018-08-28 17:20:06 +0100

    icon added

--- models/formatter.rb
+++ models/formatter.rb
@@ -4,7 +4,7 @@ class Formatter
     if number < 0
       string_number = number.to_s
       string_number[0] = ''
-      return "£(#{string_number})"
+      return "£(#{string_number.to_f.round(2)})"
     elsif number > 0
       return "£#{number}"
     else
